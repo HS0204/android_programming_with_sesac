b7432ef2a8800e2f973847c7d1d8da87507b36da
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-08-04 00:33:01 +0900

    findViewById -> ViewBinding

--- firstweek/app/src/main/java/hs/project/firstweek/MainActivity.kt
+++ firstweek/app/src/main/java/hs/project/firstweek/MainActivity.kt
@@ -7,70 +7,57 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
+import hs.project.firstweek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var domesticBtn: Button
-    lateinit var overseasBtn: Button
-    lateinit var pensionBtn: Button
-
-    lateinit var domesticView: ImageView
-    lateinit var overseasView: ImageView
-    lateinit var pensionView: ImageView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        domesticBtn = findViewById(R.id.domestic_stocks_btn)
-        overseasBtn = findViewById(R.id.overseas_stocks_btn)
-        pensionBtn = findViewById(R.id.pension_btn)
+        binding.domesticStocksBtn.setOnClickListener {
+            binding.domesticStocksView.visibility = View.VISIBLE
+            binding.domesticStocksBtn.setTextColor(Color.parseColor("#1F1F1F"))
+            binding.domesticStocksBtn.setTypeface(null, Typeface.BOLD)
 
-        domesticView = findViewById(R.id.domestic_stocks_view)
-        overseasView = findViewById(R.id.overseas_stocks_view)
-        pensionView = findViewById(R.id.pension_view)
+            binding.overseasStocksView.visibility = View.INVISIBLE
+            binding.overseasStocksBtn.setTextColor(Color.parseColor("#434343"))
+            binding.overseasStocksBtn.setTypeface(null, Typeface.NORMAL)
 
-        domesticBtn.setOnClickListener {
-            domesticView.visibility = View.VISIBLE
-            domesticBtn.setTextColor(Color.parseColor("#1F1F1F"))
-            domesticBtn.setTypeface(null, Typeface.BOLD)
-
-            overseasView.visibility = View.INVISIBLE
-            overseasBtn.setTextColor(Color.parseColor("#434343"))
-            overseasBtn.setTypeface(null, Typeface.NORMAL)
-
-            pensionView.visibility = View.INVISIBLE
-            pensionBtn.setTextColor(Color.parseColor("#434343"))
-            pensionBtn.setTypeface(null, Typeface.NORMAL)
+            binding.pensionView.visibility = View.INVISIBLE
+            binding.pensionBtn.setTextColor(Color.parseColor("#434343"))
+            binding.pensionBtn.setTypeface(null, Typeface.NORMAL)
 
         }
-        overseasBtn.setOnClickListener {
-            domesticView.visibility = View.INVISIBLE
-            domesticBtn.setTextColor(Color.parseColor("#434343"))
-            domesticBtn.setTypeface(null, Typeface.NORMAL)
+        binding.overseasStocksBtn.setOnClickListener {
+            binding.domesticStocksView.visibility = View.INVISIBLE
+            binding.domesticStocksBtn.setTextColor(Color.parseColor("#434343"))
+            binding.domesticStocksBtn.setTypeface(null, Typeface.NORMAL)
 
-            overseasView.visibility = View.VISIBLE
-            overseasBtn.setTextColor(Color.parseColor("#1F1F1F"))
-            overseasBtn.setTypeface(null, Typeface.BOLD)
+            binding.overseasStocksView.visibility = View.VISIBLE
+            binding.overseasStocksBtn.setTextColor(Color.parseColor("#1F1F1F"))
+            binding.overseasStocksBtn.setTypeface(null, Typeface.BOLD)
 
-            pensionView.visibility = View.INVISIBLE
-            pensionBtn.setTextColor(Color.parseColor("#434343"))
-            pensionBtn.setTypeface(null, Typeface.NORMAL)
+            binding.pensionView.visibility = View.INVISIBLE
+            binding.pensionBtn.setTextColor(Color.parseColor("#434343"))
+            binding.pensionBtn.setTypeface(null, Typeface.NORMAL)
         }
-        pensionBtn.setOnClickListener {
-            domesticView.visibility = View.INVISIBLE
-            domesticBtn.setTextColor(Color.parseColor("#434343"))
-            domesticBtn.setTypeface(null, Typeface.NORMAL)
+        binding.pensionBtn.setOnClickListener {
+            binding.domesticStocksView.visibility = View.INVISIBLE
+            binding.domesticStocksBtn.setTextColor(Color.parseColor("#434343"))
+            binding.domesticStocksBtn.setTypeface(null, Typeface.NORMAL)
 
-            overseasView.visibility = View.INVISIBLE
-            overseasBtn.setTextColor(Color.parseColor("#434343"))
-            overseasBtn.setTypeface(null, Typeface.NORMAL)
+            binding.overseasStocksView.visibility = View.INVISIBLE
+            binding.overseasStocksBtn.setTextColor(Color.parseColor("#434343"))
+            binding.overseasStocksBtn.setTypeface(null, Typeface.NORMAL)
 
-            pensionView.visibility = View.VISIBLE
-            pensionBtn.setTextColor(Color.parseColor("#1F1F1F"))
-            pensionBtn.setTypeface(null, Typeface.BOLD)
+            binding.pensionView.visibility = View.VISIBLE
+            binding.pensionBtn.setTextColor(Color.parseColor("#1F1F1F"))
+            binding.pensionBtn.setTypeface(null, Typeface.BOLD)
         }
 
     }
