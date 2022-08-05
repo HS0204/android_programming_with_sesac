c579ff08b5a34b2f5313ea42929c6aa80484ff12
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-08-05 16:08:37 +0900

    하단 바, 프래그먼트 기능 구현

--- secondweek/app/src/main/java/hs/project/secondweek/MainActivity.kt
+++ secondweek/app/src/main/java/hs/project/secondweek/MainActivity.kt
@@ -3,10 +3,15 @@ package hs.project.secondweek
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import hs.project.secondweek.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -26,9 +31,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         homeFragment = HomeFragment.newInstance()
-        supportFragmentManager.beginTransaction().add(R.id.view_section, homeFragment).commit()
+        supportFragmentManager.beginTransaction().add(binding.viewSection.id, homeFragment).commit()
 
-        binding.bottomNavigation.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
 
     }
 
@@ -64,35 +69,71 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private val onBottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-        when(it.itemId){
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+        when(item.itemId){
             R.id.menu_home -> {
                 Log.d(TAG, "MainActivity - 홈 클릭")
                 homeFragment = HomeFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.view_section, homeFragment).commit()
+                transaction.replace(binding.viewSection.id, homeFragment, "home")
             }
             R.id.menu_music4U -> {
                 Log.d(TAG, "MainActivity - 뮤직4U 클릭")
                 music4UFragment = Music4UFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.view_section, music4UFragment).commit()
+                transaction.replace(binding.viewSection.id, music4UFragment, "music4U")
             }
             R.id.menu_my_music -> {
                 Log.d(TAG, "MainActivity - 내 음악 클릭")
                 myMusicFragment = MyMusicFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.view_section, myMusicFragment).commit()
+                transaction.replace(binding.viewSection.id, myMusicFragment, "myMusic")
             }
             R.id.menu_searching -> {
                 Log.d(TAG, "MainActivity - 탐색 클릭")
                 searchingFragment = SearchingFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.view_section, searchingFragment).commit()
+                transaction.replace(binding.viewSection.id, searchingFragment, "searching")
             }
             R.id.menu_always -> {
                 Log.d(TAG, "MainActivity - 24/7 클릭")
                 alwaysFragment = AlwaysFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.view_section, alwaysFragment).commit()
+                transaction.replace(binding.viewSection.id, alwaysFragment, "always")
             }
         }
-        true
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+        return true
+    }
+
+    private fun updateBottomMenu(navigation: BottomNavigationView) {
+        Log.d(TAG, "MainActivity - updateBottomMenu() 호출")
+
+        val homeTag: Fragment? = supportFragmentManager.findFragmentByTag("home")
+        val music4UTag: Fragment? = supportFragmentManager.findFragmentByTag("music4U")
+        val myMusicTag: Fragment? = supportFragmentManager.findFragmentByTag("myMusic")
+        val searchingTag: Fragment? = supportFragmentManager.findFragmentByTag("searching")
+        val alwaysTag: Fragment? = supportFragmentManager.findFragmentByTag("always")
+
+        if(homeTag != null && homeTag.isVisible) {navigation.menu.findItem(R.id.menu_home).isChecked = true }
+        if(music4UTag != null && music4UTag.isVisible) {navigation.menu.findItem(R.id.menu_music4U).isChecked = true }
+        if(myMusicTag != null && myMusicTag.isVisible) {navigation.menu.findItem(R.id.menu_my_music).isChecked = true }
+        if(searchingTag != null && searchingTag.isVisible) {navigation.menu.findItem(R.id.menu_searching).isChecked = true }
+        if(alwaysTag != null && alwaysTag.isVisible) {navigation.menu.findItem(R.id.menu_always).isChecked = true }
+    }
+
+    override fun onBackPressed() {
+        Log.d(TAG, "MainActivity - onBackPressed() 호출")
+        Log.d(TAG, "현재 스택의 수는 ${supportFragmentManager.backStackEntryCount}")
+
+        if(supportFragmentManager.backStackEntryCount == 0){
+            onStop()
+            onDestroy()
+            System.runFinalization()
+            exitProcess(0)
+        }
+
+        super.onBackPressed()
+        updateBottomMenu(binding.bottomNavigation)
     }
 
 }
